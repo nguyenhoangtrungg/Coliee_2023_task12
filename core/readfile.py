1fec0d565894f16117493c96942d320d10ce9a12
Trung <20020083@vnu.edu.vn>
2023-04-17 14:44:36 +0700

    update comment readfile

--- core/readfile.py
+++ core/readfile.py
@@ -1,13 +1,21 @@
 import os
 import json
 
+"""
+@param filename: link to file
+@return: content in json file
+"""
 def read_jsonfile(filename):
     if os.path.exists(filename):
         with open(filename, 'r') as f:
             return json.load(f)
     else:
         return []
-    
+
+"""
+@param filename: link to file
+@return: content in txt file
+"""
 def read_txtfile(filename):
     if os.path.exists(filename):
         with open(filename, 'r', encoding = "utf8") as f:
@@ -15,9 +23,19 @@ def read_txtfile(filename):
     else:
         return ""
     
+"""
+@param filename: link to file
+@return: list of file in folder
+"""
 def get_folder(filename):
     return os.listdir(filename)
 
+"""
+write data to json file
+
+@param filename: link to file
+@param data: data to write
+"""
 def write_jsonfile(filename, data):
     result = json.dumps(data, indent=2, ensure_ascii=False)
     myjsonfile = open(filename, "w", encoding="utf8")
